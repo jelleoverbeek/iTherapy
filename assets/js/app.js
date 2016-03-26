64328c52778f26130ae2285250d019f01e09234f
Jelle Overbeek <overbeekjelle@gmail.com>
2016-03-26 19:04:46 +0100

    Added cancel modals

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,3 +2,29 @@
  * Created by jelleoverbeek on 26-03-16.
  */
 
+$(function() {
+
+    $("#cancel-13dec").click(function () {
+        $("#cancel-13dec-modal").fadeIn(300);
+    });
+
+    $(".cancel-13dec-submit").click( function() {
+        $("#cancel-13dec").hide();
+        $("#cancel-13dec-status").css('display', 'inline-block');
+        $(".13dec").addClass("disabled");
+
+        $("#cancel-13dec-modal").fadeOut(300);
+    });
+
+    $("#cancel-20dec").click(function () {
+        $("#cancel-20dec-modal").fadeIn(300);
+    });
+
+    $(".cancel-20dec-submit").click( function() {
+        $("#cancel-20dec").hide();
+        $("#cancel-20dec-status").css('display', 'inline-block');
+        $(".20dec").addClass("disabled");
+
+        $("#cancel-20dec-modal").fadeOut(300);
+    });
+});
